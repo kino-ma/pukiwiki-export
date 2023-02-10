ce476b71cc224bddcbf2a7a3bd95d4a3aa963ab7
kino-ma <ma@kino.ma>
2023-02-10 14:14:35 +0900

    build: add nix environment

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "pukiwiki-to-growi";
+
+  inputs = {
+    nixpkgs = { url = "github:NixOS/nixpkgs/nixpkgs-unstable"; };
+    flake-utils = { url = "github:numtide/flake-utils"; };
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: 
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        inherit (nixpkgs.lib) optional;
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            python310
+            python310Packages.black
+          ];
+        };
+      });
+}
